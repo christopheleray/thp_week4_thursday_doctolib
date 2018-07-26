e7a7f77343682d654a602008e085fd274926e3a4
christopheleray <christophe.leray1@gmail.com>
2018-07-26 14:19:12 +0200

    creation des model et seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+
+10.times do 
+  doctor =  Doctor.create(first_name: Faker::Name.name, last_name: Faker::Name.name, specialty: Faker::Superhero.power, postal_code: Faker::Number.number(5))
+end
+  
+10.times do
+  patient = Patient.create(first_name: Faker::Name.name, last_name: Faker::Name.name)
+end
+
+10.times do
+  appointmeent= Appointment.create(date: Faker::Date.forward(90).to_datetime, doctor_id: rand((Doctor.first.id)..(Doctor.last.id)), patient_id: rand((Patient.first.id)..(Patient.last.id)))
+end
